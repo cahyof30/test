3c36b51aa63ddcffe81c2c054f47edf66d51d058
cahyof30 <f30.cahyo@gmail.com>
2022-10-10 20:59:20 +0700

    update pertemuan 5

--- application/views/v_header.php
+++ application/views/v_header.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <title>Web Prog II | Merancang Template sederhana dengan codeigniter</title>
     <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/stylebuku.css">
 </head>
+
 <body>
     <div id="wrapper">
         <header>
@@ -14,8 +16,9 @@
             </hgroup>
             <nav>
                 <ul>
-                    <li><a href="<?php echo base_url().'index.php/web' ?>">Home</a></li>
-                    <li><a href="<?php echo base_url().'index.php/web/about' ?>">About</a></li>
+                    <li><a href="<?php echo base_url() . 'index.php/web' ?>">Home</a></li>
+                    <li><a href="<?php echo base_url() . 'index.php/web/about' ?>">About</a></li>
+                    <li><a href="<?php echo base_url() . 'index.php/matakuliah' ?>">Mata Kuliah</a></li>
                 </ul>
             </nav>
             <div class="clear"></div>
